cb1a66ef6155e89d9b14727e50d2e19883aac9ed
vkkurre <vkantkurre@gmail.com>
2017-02-15 20:03:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res){
+    res.send("Article One has requested");
+});
+
+app.get('/article-two',function(req,res){
+    res.send("Article Two has requested");
+});
+
+app.get('/article-three',function(req,res){
+    res.send("Article Tgree has requested");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
